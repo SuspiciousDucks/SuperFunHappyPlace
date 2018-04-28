304147ccfc023cd8ac403ef71084f8bb6c07572f
Jacob Green <jakey113g@hotmail.co.uk>
2018-04-28 23:47:14 +0100

    Hopefully updated and renamed everything to Daisy_Game
    I also added in an icon for the program as a helpful gesture.

---
+++ Daisy_Game/Assets/Scripts/Gem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+
+    void OnMouseDown()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        if(spriteRenderer.color == Color.blue)
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            spriteRenderer.color = Color.blue;
+        }
+       
+    }
+}   
